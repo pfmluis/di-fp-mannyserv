692421b796872e629db785c8565a4ffc2d05dd06
Pedro <pfmluis@gmail.com>
2019-01-25 16:07:18 +0000

    Added Filters

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { professionals } from './../../data/professionals';
 
 import { SearchFilter } from './../../classes/search-filter';
+import { Professional } from 'src/app/classes/professional';
 
 
 @Component({
@@ -13,16 +15,61 @@ import { SearchFilter } from './../../classes/search-filter';
 export class SearchPageComponent implements OnInit {
 
   p = 1;
-  collection: any[] = professionals;
-  result: SearchFilter;
-  constructor() { }
+  filtred = false;
+  collection: Professional[] = professionals;
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const profession = this.route.snapshot.params.content;
+    this.filterResults(new SearchFilter(profession, null, null, Number.MAX_SAFE_INTEGER));
   }
 
   onSubmit(result: SearchFilter): void {
-    console.log(result);
-    this.result = result;
+    this.filterResults(result);
   }
 
+  filterResults(result: SearchFilter): void {
+    this.collection = [];
+    if (result) {
+      professionals.forEach(professional => {
+        if (result.profession && result.location) {
+          if (
+              professional.speciallity.includes(result.profession) &&
+              professional.price <= result.price &&
+              professional.location === result.location ||
+              professional.tags.includes(result.profession)  &&
+              professional.price <= result.price &&
+              professional.location === result.location
+              ) {
+              this.collection.push(professional);
+            }
+        } else if (result.profession && !result.location) {
+          if (
+            professional.speciallity.includes(result.profession) &&
+            professional.price <= result.price ||
+            professional.tags.includes(result.profession)  &&
+            professional.price <= result.price
+            ) {
+            this.collection.push(professional);
+          }
+        } else if (!result.profession && result.location) {
+          if (
+            professional.price <= result.price &&
+            professional.location === result.location ||
+            professional.price <= result.price &&
+            professional.location === result.location
+            ) {
+            this.collection.push(professional);
+        } else {
+          if (professional.price <= result.price) {
+              this.collection.push(professional);
+          }
+        }
+      }
+    });
+    } else {
+      this.collection = professionals;
+    }
+  }
 }
